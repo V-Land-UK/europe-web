3b332b52071adcb365f19fcaece41907a3e56c6a
sam-20 <botwesamuel192@gmail.com>
2022-11-01 23:57:13 +0000

    added package.json

--- pages/_app.js
+++ pages/_app.js
@@ -15,22 +15,21 @@ Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <ChakraProvider>
-    //   <GlobalProvider>
-    //     <Script
-    //       id="Adsense-id"
-    //       async
-    //       onError={(err) => {
-    //         console.error("Script failed to load", err);
-    //       }}
-    //       strategy="afterInteractive"
-    //       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9166716457553506"
-    //       crossorigin="anonymous"
-    //     ></Script>
-    //     <Component {...pageProps} />
-    //   </GlobalProvider>
-    // </ChakraProvider>
-    <p>Hello worlds</p>
+    <ChakraProvider>
+      <GlobalProvider>
+        <Script
+          id="Adsense-id"
+          async
+          onError={(err) => {
+            console.error("Script failed to load", err);
+          }}
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9166716457553506"
+          crossorigin="anonymous"
+        ></Script>
+        <Component {...pageProps} />
+      </GlobalProvider>
+    </ChakraProvider>
   );
 }
 
